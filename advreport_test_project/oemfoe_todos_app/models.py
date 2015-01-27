11ca934a0d25a8e6c6816fe42b364107460bb94e
Jef Geskens <jef.geskens@gmail.com>
2015-01-27 14:10:17 +0100

    Give some love to the test project

---
+++ advreport_test_project/oemfoe_todos_app/models.py
@@ -0,0 +1,23 @@
+from __future__ import unicode_literals
+from django.contrib.auth.models import User
+from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
+
+
+@python_2_unicode_compatible
+class TodoList(models.Model):
+    owner = models.ForeignKey(User)
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return '{0} owned by {1}'.format(self.name, self.owner.get_full_name())
+
+
+@python_2_unicode_compatible
+class TodoItem(models.Model):
+    todo_list = models.ForeignKey(TodoList)
+    name = models.CharField(max_length=255)
+    done = models.DateTimeField(blank=True, null=True)
+
+    def __str__(self):
+        return '{0} from {1}'.format(self.name, self.todo_list)
