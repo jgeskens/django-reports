06f6f7303709909890d45701ccb7b7c0c55940ea
Jakub Paczkowski <jakub@paczkowski.eu>
2015-02-24 12:36:35 +0100

    fix tests for multiple users

--- tests/test_reports.py
+++ tests/test_reports.py
@@ -7,6 +7,7 @@ from advanced_reports.defaults import AdvancedReport
 
 class AdvancedReportTest(TestCase):
     def setUp(self):
+        User.objects.create_user("test2", "test2@example.com", "foobar")
         self.report = AdvancedReport()
         self.report.models = (User,)
 
@@ -14,5 +15,5 @@ class AdvancedReportTest(TestCase):
         self.assertQuerysetEqual(User.objects.order_by('pk'), self.report.get_sorted_queryset('__unicode__'), transform=lambda x:x)
         self.assertQuerysetEqual(User.objects.order_by('pk'), self.report.get_sorted_queryset('__str__'), transform=lambda x:x)
         self.assertQuerysetEqual(User.objects.order_by('first_name'), self.report.get_sorted_queryset('first_name'), transform=lambda x:x)
-        self.assertQuerysetEqual(User.objects.all(), self.report.get_sorted_queryset('field_that_does_not_exist'), transform=lambda x:x)
-        self.assertQuerysetEqual(User.objects.all(), self.report.get_sorted_queryset('field_that__does_not_exist'), transform=lambda x:x)
+        self.assertQuerysetEqual(User.objects.all(), self.report.get_sorted_queryset('field_that_does_not_exist'), transform=lambda x:x, ordered=False)
+        self.assertQuerysetEqual(User.objects.all(), self.report.get_sorted_queryset('field_that__does_not_exist'), transform=lambda x:x, ordered=False)
